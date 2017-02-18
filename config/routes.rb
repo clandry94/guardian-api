c11f56e4ef5106cadab20f9ce561cd3e4503914c
Conor Landry <clandry94@ufl.edu>
2017-02-18 16:31:32 -0500

    FIxed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tmp_endpoint#out'  
+  root to: 'tmp_endpoint#out'
 
   post 'test', to: 'tmp_endpoint#in'
   get 'test', to: 'tmp_endpoint#out'
-<<<<<<< HEAD
-  
-  get 'resp', to: 'application#getkey'
-  #post 'auth', to: 'application#new'
-=======
   #User routes
-  post 'user', to: 'user#show' 
   post 'user', to: 'user#update'
-  post 'user', to: 'user#delete'
+  delete 'user', to: 'user#delete'
   get 'user', to: 'user#show'
   # notifications params 'phone_number':'xxxxxxxxx', 'alert_message':'xxxxxxxx'
-  post 'notifications', to: 'notifications#send_message'
->>>>>>> user_controller
+  post 'notifications/send_message', to: 'notifications#send_message'
+  post 'notifications/send_email', to: 'notifications#send_email'
 end
