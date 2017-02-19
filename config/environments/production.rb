060846380a1a45b080006dadd5a720a27564363d
Conor Landry <clandry94@ufl.edu>
2017-02-19 03:12:37 -0500

    removed bad config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,13 +74,13 @@ Rails.application.configure do
   end
 end
 
-config.action_mailer.delivery_method = :smtp
+#config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "smtp.gmail.com",
- :port                 => 587,
- :user_name            => ENV['gmail_username'],
- :password             => ENV['gmail_password'],
- :authentication       => "plain",
-:enable_starttls_auto => true
-}
+#config.action_mailer.smtp_settings = {
+ #:address              => "smtp.gmail.com",
+ #:#port                 => 587,
+# :user_name            => ENV['gmail_username'],
+# :password             => ENV['gmail_password'],
+# :authentication       => "plain",
+#:enable_starttls_auto => true
+#}
