2355c37b4dde491bfc8b81c5fe439208aa575068
Joseph Pena <josephpendev@gmail.com>
2017-02-19 01:27:41 -0500

    added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,208 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([
+    {
+	"id_token": "awkotaco12",
+	"access_token": "wdokokwadok21",
+	"refresh_token": "wdkajjpokfap32",
+	"name": {
+		"first_name": "Joseph",
+		"last_name": "Pena"
+	},
+	"email": "joe@pena.gov",
+	"address": {
+		"street": "111 Noob ave",
+		"city": "Gainesville",
+		"state": "Florida",
+		"zip": "32603"
+	},
+	"phone": "2817775729",
+	"emergency_contacts": [{
+		"name": {
+			"first_name": "Conor",
+			"last_name": "Landry"
+		},
+		"email": "conor@landry.com",
+		"relation": "Uncle",
+		"priority": "1"
+	}, {
+		"name": {
+			"first_name": "Joseph",
+			"last_name": "Pena"
+		},
+		"email": "joe@pena.com",
+		"relation": "Father",
+		"priority": "2"
+	}, {
+		"name": {
+			"first_name": "Jacob",
+			"last_name": "Ville"
+		},
+		"email": "jacob@ville.com",
+		"relation": "Mother",
+		"priority": "3"
+	}],
+	"events": [{
+		"title": "Motel 8 Meetup",
+		"start_date": "2017-2-18",
+		"end_date": "2017-2-18",
+		"start_time": "800",
+		"end_time": "1900",
+		"geo_points": {
+			"longitude": "123.024",
+			"latitude": "-24.123"
+		},
+		"location_name": "Motel 8 in a Sketchy Place",
+		"checked_in": "0"
+	}, {
+		"title": "Motel 12 Meetup",
+		"start_date": "2017-3-18",
+		"end_date": "2017-3-18",
+		"start_time": "1100",
+		"end_time": "2100",
+		"geo_points": {
+			"longitude": "120.024",
+			"latitude": "-17.123"
+		},
+		"location_name": "Motel 8 in a Sketchy Place",
+		"checked_in": "0"
+	}]
+},
+    {
+	"id_token": "awekpo4p1j2ij",
+	"access_token": "wdokokwadok",
+	"refresh_token": "wdkajjpokfap",
+	"name": {
+		"first_name": "Sergio",
+		"last_name": "Puleri"
+	},
+	"email": "sergio@puleri.gov",
+	"address": {
+		"street": "111 Fake Street",
+		"city": "Gainesville",
+		"state": "Florida",
+		"zip": "32603"
+	},
+	"phone": "2817775729",
+	"emergency_contacts": [{
+		"name": {
+			"first_name": "Conor",
+			"last_name": "Landry"
+		},
+		"email": "conor@landry.com",
+		"relation": "Uncle",
+		"priority": "1"
+	}, {
+		"name": {
+			"first_name": "Joseph",
+			"last_name": "Pena"
+		},
+		"email": "joe@pena.com",
+		"relation": "Father",
+		"priority": "2"
+	}, {
+		"name": {
+			"first_name": "Jacob",
+			"last_name": "Ville"
+		},
+		"email": "jacob@ville.com",
+		"relation": "Mother",
+		"priority": "3"
+	}],
+	"events": [{
+		"title": "Motel 8 Meetup",
+		"start_date": "2017-2-18",
+		"end_date": "2017-2-18",
+		"start_time": "800",
+		"end_time": "1900",
+		"geo_points": {
+			"longitude": "123.024",
+			"latitude": "-24.123"
+		},
+		"location_name": "Motel 8 in a Sketchy Place",
+		"checked_in": "0"
+	}, {
+		"title": "Motel 12 Meetup",
+		"start_date": "2017-3-18",
+		"end_date": "2017-3-18",
+		"start_time": "1100",
+		"end_time": "2100",
+		"geo_points": {
+			"longitude": "120.024",
+			"latitude": "-17.123"
+		},
+		"location_name": "Motel 8 in a Sketchy Place",
+		"checked_in": "0"
+	}]
+},
+    {
+	"id_token": "kakopokako",
+	"access_token": "wdokokwadok",
+	"refresh_token": "wdkajjpokfap",
+	"name": {
+		"first_name": "Jacob",
+		"last_name": "Ville"
+	},
+	"email": "sergio@puleri.gov",
+	"address": {
+		"street": "111 Fake Street",
+		"city": "Gainesville",
+		"state": "Florida",
+		"zip": "32603"
+	},
+	"phone": "2817775729",
+	"emergency_contacts": [{
+		"name": {
+			"first_name": "Conor",
+			"last_name": "Landry"
+		},
+		"email": "conor@landry.com",
+		"relation": "Uncle",
+		"priority": "1"
+	}, {
+		"name": {
+			"first_name": "Joseph",
+			"last_name": "Pena"
+		},
+		"email": "joe@pena.com",
+		"relation": "Father",
+		"priority": "2"
+	}, {
+		"name": {
+			"first_name": "Jacob",
+			"last_name": "Ville"
+		},
+		"email": "jacob@ville.com",
+		"relation": "Mother",
+		"priority": "3"
+	}],
+	"events": [{
+		"title": "Motel 8 Meetup",
+		"start_date": "2017-2-18",
+		"end_date": "2017-2-18",
+		"start_time": "800",
+		"end_time": "1900",
+		"geo_points": {
+			"longitude": "123.024",
+			"latitude": "-24.123"
+		},
+		"location_name": "Motel 8 in a Sketchy Place",
+		"checked_in": "0"
+	}, {
+		"title": "Motel 12 Meetup",
+		"start_date": "2017-3-18",
+		"end_date": "2017-3-18",
+		"start_time": "1100",
+		"end_time": "2100",
+		"geo_points": {
+			"longitude": "120.024",
+			"latitude": "-17.123"
+		},
+		"location_name": "Motel 8 in a Sketchy Place",
+		"checked_in": "0"
+	}]
+ }
+])
+
