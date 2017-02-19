d2af4d3f20c10a37b69f0291cebcb8f6d64ecbe5
Conor Landry <clandry94@ufl.edu>
2017-02-18 21:52:41 -0500

    Added user_params

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,12 +7,12 @@ class UserController < ApplicationController
 
     @user = User.find_by(id_token: id_token)
     Rails.logger.info User.find_by(id_token: id_token)
-    @user.update_attributes!(updateFields.permit(:role_ids))
+    @user.update_attributes!(user_params)
     @user.save   
     render json: @user
   end
 
- def show
+  def show
     render json: @user
   end
 
@@ -26,13 +26,25 @@ class UserController < ApplicationController
   def events
     foundUserEvents = @user[:events]
     render json: foundUserEvents
-
+  
+  end
+  
   def user_creation_callback
     Rails.logger.info params[:access_token]
     new_user = User.new(id_token: params[:id_token],
                         access_token: params[:access_token],
                         refresh_token: params[:refresh_token])
   end
-end
+
+  private
+
+  def user_params
+    params.permit(:access_token, 
+                  :refresh_token, 
+                  :photo, 
+                  :phone, 
+                  :emergency_contacts, 
+                  :address)
+  end
 end
 
